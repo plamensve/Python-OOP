5ebe1a9d6fe9de27d4207c79d408dcf3f30ab394
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-02-24 09:33:50 +0200

    Python OOP - 24.02.2024

--- 02_EXCERCISE_CLASSES_AND_OBJECTS/time.py
+++ 02_EXCERCISE_CLASSES_AND_OBJECTS/time.py
@@ -9,14 +9,32 @@ class Time:
         self.seconds = seconds
 
     def set_time(self, hours, minutes, seconds):
-        pass
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 
     def get_time(self):
-        return f"{hh}:{mm}:{ss}"
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
     def next_second(self):
-        pass
+        self.seconds += 1
+        self.update_valid_time()
+        return self.get_time()
+
+    def update_valid_time(self):
+        if self.seconds >= Time.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+
+            if self.minutes >= Time.max_minutes:
+                self.minutes = 0
+                self.hours += 1
+
+                if self.hours >= Time.max_hours:
+                    self.hours = 0
 
 
-time = Time(9, 30, 59)
+time = Time(23, 59, 59)
 print(time.next_second())
+
+
